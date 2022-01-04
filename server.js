1657099195c8921d01adcc800ae2f2c1b9d8c9ea
MicaelaDZ <micaelazampone@gmail.com>
2022-01-04 13:23:24 -0300

    hosting

--- server.js
+++ server.js
@@ -5,12 +5,20 @@ const express = require("express")
 const cors = require("cors")
 const app = express()
 require('./config/database')
+const path = require('path');
 
 
 app.use(cors())
 app.use(express.json()) 
 app.use(passport.initialize())
 app.use("/api", Router)
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static("./client/build"))
+    app.get("*", (req,res)=> {
+        res.sendFile(path.join(__dirname+'client/build/index.html'))
+    })
+}
 
 app.listen(process.env.PORT || 4000, '0.0.0.0', ()=>{
     console.log("Inicializando...")
